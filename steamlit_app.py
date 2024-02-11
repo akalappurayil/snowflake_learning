ed4efe4ea391f3cd8fcd7c296c8ac022b92310ba
akalappurayil <110657172+akalappurayil@users.noreply.github.com>
2024-02-12 05:27:42 +0530

    Update steamlit_app.py
    Fixed the quote issue

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents healthy DIner') 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Blueberry Oatmeal)
+streamlit.text('Omega3 & Blueberry Oatmeal')
 streamlit.text('Kale, spinach & Oat smoothie')
 streamlit.text('Hard boiled free range egg')
 
