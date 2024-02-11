65feb8021c64be8099f4b660b5417c5bc81ba147
akalappurayil <110657172+akalappurayil@users.noreply.github.com>
2024-02-12 05:27:12 +0530

    Update steamlit_app.py
    Added breakfast menu

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title('My Parents healthy DIner') 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry Oatmeal)
+streamlit.text('Kale, spinach & Oat smoothie')
+streamlit.text('Hard boiled free range egg')
 
