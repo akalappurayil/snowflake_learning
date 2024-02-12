98b291dfd6e93ced53365dd66fdc231d407c1985
akalappurayil <110657172+akalappurayil@users.noreply.github.com>
2024-02-12 05:31:48 +0530

    Update steamlit_app.py
    Updated menu

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 streamlit.title('My Parents healthy DIner') 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach & Oat smoothie')
-streamlit.text('Hard boiled free range egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, spinach & Oat smoothie')
+streamlit.text('🐔 Hard boiled free range egg')
+streamlit.text('🥑 Avacado toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
