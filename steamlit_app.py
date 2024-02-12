8ed9d964b956c715b475dcf1b705fdc236e8314d
akalappurayil <110657172+akalappurayil@users.noreply.github.com>
2024-02-12 06:13:43 +0530

    Update steamlit_app.py
    Removed filter

--- steamlit_app.py
+++ steamlit_app.py
@@ -16,4 +16,4 @@ my_fruit_list.set_index('Fruit', inplace=True)
 fruits_selected = streamlit.multiselect('Pick some fruits : ', list(my_fruit_list.index), ['Avocado','Strawberries']) 
 fruits_to_show = my_fruit_list.loc(fruits_selected)
 #Display the list of fruits
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(my_fruit_list)
