6e899e0f6b33953d1dafd3e4801cf28a7f8b1161
akalappurayil <110657172+akalappurayil@users.noreply.github.com>
2024-02-12 06:09:58 +0530

    Update steamlit_app.py
    index inpace=true

--- steamlit_app.py
+++ steamlit_app.py
@@ -10,10 +10,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-#my_fruit_list.set_index('Fruit')
+#my_fruit_list.set_index('Fruit', inplace=True)
 
 #Let's add an option for users to pick the fruits for their own smoothie
 fruits_selected = streamlit.multiselect('Pick some fruits : ', list(my_fruit_list.Fruit), ['Avocado','Strawberries']) 
 fruits_to_show = my_fruit_list.loc(fruits_selected)
 #Display the list of fruits
-streamlit.dataframe(fruits_to_show)
+#streamlit.dataframe(fruits_to_show)
