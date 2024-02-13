8c0c1e0834fb42b4d8c5a26464b7b12e48d35534
akalappurayil <110657172+akalappurayil@users.noreply.github.com>
2024-02-13 13:58:24 +0530

    Update steamlit_app.py
    Added a fruit choice variable

--- steamlit_app.py
+++ steamlit_app.py
@@ -21,8 +21,10 @@ streamlit.dataframe(fruits_to_show)
 
 #new section for API response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 streamlit.text(fruityvice_response.json())
 
 # Load the json response to a dataframe and normalize
