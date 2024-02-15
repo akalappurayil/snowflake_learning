ff07ac84812376bf72eafc4fe3c37ec79f9866e0
akalappurayil <110657172+akalappurayil@users.noreply.github.com>
2024-02-15 07:43:53 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -27,9 +27,16 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 streamlit.text(fruityvice_response.json())
-import snowflake.connector 
+
 
 # Load the json response to a dataframe and normalize
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # Show the parsed json data as a table
 streamlit.dataframe(fruityvice_normalized)
+import snowflake.connector 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
